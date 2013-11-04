8ac01bf6b914762c65523775f7ec36bb199f90de
clarkmatt <clark.matthew8@gmail.com>
2013-11-04 15:31:31 -0700

    comment added by matt

--- baxter_barcode_pickup/src/test.cpp
+++ baxter_barcode_pickup/src/test.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 //test
 int main(int argc, char** argv){
-    std::cout << "Hello world.\n" << "And the rest of the universe\n"; 
+    std::cout << "Hello world.\n" << "And the rest of the universe\n" << "and the multiverse\n"; 
     return 0;
 };
