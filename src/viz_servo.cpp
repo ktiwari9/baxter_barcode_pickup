9fa3dcc36c932c74e18eb182f7b2c864d917cc3d
Peter <peter.jeffris@colorado.edu>
2013-12-07 16:38:25 -0700

    epic fail

--- src/viz_servo.cpp
+++ src/viz_servo.cpp
@@ -6,6 +6,8 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <tf/transform_listener.h>
+#include <iostream>
 
 class VisualServo
 {
@@ -17,17 +19,28 @@ private:
   boost::scoped_ptr<move_group_interface::MoveGroup> move_group_;
   std::string arm_;
   std::string planning_group_name_;
+  std::string hand_camera_;
   std::string camera_image_;
   tf::StampedTransform target_;
-  //std::vector<geometry_msgs::Pose> target_;
+  double error_x_;
+  double error_y_;
+  std::vector<double> last_rpy_;
+  std::vector<double> next_rpy_;
+  tf::TransformListener tf_listener_;
+  tf::StampedTransform tf_transform_;
+  // planning_interface::MotionPlanRequest req;
+  // planning_interface::MotionPlanResponse res;
+  // planning_interface::PlannerManagerPtr planner_instance;
 public:
   VisualServo() : transporter_(node_),  
 		  arm_("left"),
 		  planning_group_name_(arm_+"_arm"),
-		  camera_image_("/cameras/"+arm_+"_hand_camera/image")
+		  hand_camera_(arm_+"_hand_camera"),
+		  camera_image_("/cameras/"+hand_camera_+"/image")
+		  
   {
     move_group_.reset(new move_group_interface::MoveGroup(planning_group_name_));
-    move_group_->setPlanningTime(20.0);
+    move_group_->setPlanningTime(10.0);
     move_group_->setPlannerId("RRTstarkConfigDefault");
     image_pub_ = transporter_.advertise("image_tracker", 1);
     image_sub_ = transporter_.subscribe(camera_image_, 1, &VisualServo::tracker, this);
@@ -41,49 +54,24 @@ public:
 
   bool servo_to_tag()
   	{
-	      move_group_->setRandomTarget();
+	  tf_listener_.lookupTransform("/base","/left_hand_camera", ros::Time(0), tf_transform_);
+	  geometry_msgs::PoseStamped pose;
+	  std::cout << error_x_ << "," << error_y_ << "\n";
+	  std::cout << tf_transform_.getOrigin().x() << "," << tf_transform_.getOrigin().y() << "," << tf_transform_.getOrigin().z()  << "\n";
+	  if (error_x_ < 600 || error_x_ > 5 || error_y_ < 600 || error_x_ > 5)
+	    {
+	      pose.header.frame_id = "base";  	
+	      pose.pose.position.x = tf_transform_.getOrigin().x()+error_x_*.0001;
+	      pose.pose.position.y = tf_transform_.getOrigin().y()+error_y_*.0001;
+	      pose.pose.position.z = tf_transform_.getOrigin().z();  
+	      pose.pose.orientation.x = tf_transform_.getRotation().x();
+	      pose.pose.orientation.y = tf_transform_.getRotation().y();
+	      pose.pose.orientation.z = tf_transform_.getRotation().z();
+	      pose.pose.orientation.w = tf_transform_.getRotation().w();
+	      std::cout << pose.pose.position.x << "," << pose.pose.position.y << "," << pose.pose.position.z << "\n";
+	      move_group_->setPoseTarget(pose,"left_wrist");
 	      move_group_->move();
-// bool 	setPositionTarget (double x, double y, double z, Aconst std::string &end_effector_link="")
-//  	Set the goal position of the end-effector end_effector_link to be (x, y, z). If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setRPYTarget (double roll, double pitch, double yaw, const std::string &end_effector_link="")
-//  	Set the goal orientation of the end-effector end_effector_link to be (roll,pitch,yaw) radians. If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setOrientationTarget (double x, double y, double z, double w, const std::string &end_effector_link="")
-//  	Set the goal orientation of the end-effector end_effector_link to be the quaternion (x,y,z,w). If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setPoseTarget (const Eigen::Affine3d &end_effector_pose, const std::string &end_effector_link="")
-//  	Set the goal pose of the end-effector end_effector_link. If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setPoseTarget (const geometry_msgs::Pose &target, const std::string &end_effector_link="")
-//  	Set the goal pose of the end-effector end_effector_link. If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setPoseTarget (const geometry_msgs::PoseStamped &target, const std::string &end_effector_link="")
-//  	Set the goal pose of the end-effector end_effector_link. If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setPoseTargets (const EigenSTL::vector_Affine3d &end_effector_pose, const std::string &end_effector_link="")
-//  	Set the goal pose of the end-effector end_effector_link. In this case the goal pose can be any of the ones specified in the array. If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setPoseTargets (const std::vector< geometry_msgs::Pose > &target, const std::string &end_effector_link="")
-//  	Set the goal pose of the end-effector end_effector_link. In this case the goal pose can be any of the ones specified in the array. If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// bool 	setPoseTargets (const std::vector< geometry_msgs::PoseStamped > &target, const std::string &end_effector_link="")
-//  	Set the goal pose of the end-effector end_effector_link. In this case the goal pose can be any of the ones specified in the array. If end_effector_link is empty (the default value) then the end-effector reported by getEndEffectorLink() is assumed.
- 
-// void 	setPoseReferenceFrame (const std::string &pose_reference_frame)
-//  	Specify which reference frame to assume for poses specified without a reference frame.
- 
-// bool 	setEndEffectorLink (const std::string &link_name)
-//  	Specify the link the end-effector to be considered is attached to.
- 
-// bool 	setEndEffector (const std::string &eef_name)
-//  	Specify the name of the end-effector to use.
- 
-// void 	clearPoseTarget (const std::string &end_effector_link="")
-//  	Forget pose specified for the end-effector end_effector_link.
- 
-// void 	clearPoseTargets ()
-//  	Forget any poses specified for any end-effector. 
+	    }
   	}
 
   void tracker(const sensor_msgs::ImageConstPtr& msg)
@@ -110,7 +98,11 @@ public:
     image_moment = moments(threshold_image);
     double x = image_moment.m10/image_moment.m00;
     double y = image_moment.m01/image_moment.m00;
-
+    double height_center = cv_ptr->image.rows/2;
+    double width_center = cv_ptr->image.cols/2;
+    error_y_ = (height_center-y);
+    error_x_ = (width_center-x);
+    //ROS_INFO("%f,%f,%f,%f\n",x,y,error_x_,error_y_);
     cv::imshow("Baxter Image", cv_ptr->image);
     cv::waitKey(3);
     
